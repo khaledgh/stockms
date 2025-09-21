99a38450266b7c1d085fcdcaaf2cbce23eb581b5
Khaled Ghourani <Khaledghourani4@gmail.com>
2025-09-21 11:41:19 +0300

    fixes

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=stockms3',
-    'username' => 'root',
-    'password' => 'khaled',
+    'dsn' => 'mysql:host=localhost;dbname=stock',
+    'username' => 'stock',
+    'password' => 'b2pwrsKrwFTWyMFR',
     'charset' => 'utf8mb4',
     
     // Schema cache options (for production environment)
